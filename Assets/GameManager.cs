ec9104d28f50a3c42625449172769a8df032988b
jackboy1006 <jackboy1006@gmail.com>
2019-01-28 15:23:59 +0800

    簡單主選單+按鈕

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private Transform playerList;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    //將玩家放到起始位置
+    void SetPlayerPos() {
+        
+    }
+}
---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhotonManager : Photon.PunBehaviour {
+
+    private bool Joined;
+    
+
+    // Use this for initialization
+    void Start () {
+        Joined = false;
+        PhotonNetwork.ConnectUsingSettings("TSVRv1.0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnConnectedToMaster()
+    {
+        print("正在連接大廳");
+        PhotonNetwork.JoinLobby();
+    }
+       
+
+    public override void OnJoinedLobby()
+    {
+        print("已連接至大廳");
+    }
+
+    public void JoinRoom()
+    {
+        if (!Joined) {
+            RoomOptions options = new RoomOptions { MaxPlayers = 2 };
+            print("正在創建或加入房間");
+            PhotonNetwork.JoinOrCreateRoom("gameRoom", options, TypedLobby.Default);
+            Joined = true;
+        }
+       
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("已加入房間，目前有" + PhotonNetwork.room.PlayerCount.ToString() + "人");
+    }
+
+    void CreatePlayer() {
+        
+    }
+
+    void EnterGameScene() {
+        if (PhotonNetwork.room.PlayerCount == 2) {
+            PhotonNetwork.LoadLevelAsync(1);
+        }
+    }
+}
